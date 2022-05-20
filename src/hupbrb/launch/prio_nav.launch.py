e57fddb2560c56ef291fc95dfd3bf166e375a2fe
hupbrb devs <me@example.com>
2022-05-20 15:46:31 +0200

    Small fxes

--- src/hupbrb/launch/prio_nav.launch.py
+++ src/hupbrb/launch/prio_nav.launch.py
@@ -12,14 +12,14 @@ def generate_launch_description():
     namespace_prefix = LaunchConfiguration('namespace_prefix')
     namespace_prefix_arg = DeclareLaunchArgument('namespace_prefix', 
         default_value = 'dede',
-        description='The identifier of the current robot, used in its namespace and QR code.')
+        description='A prefix to add to the identifier when generating the node namespaces.')
 
     node_id = LaunchConfiguration('id')
     node_id_arg = DeclareLaunchArgument('id', 
         description='The identifier of the current robot, used in its namespace and QR code.')
     
     # Rviz2 remap
-    rviz_node = Node(
+    priority_node = Node(
         package='hupbrb',
         executable='id_scanner',
         namespace=[namespace_prefix, node_id],
@@ -30,5 +30,6 @@ def generate_launch_description():
     ld = LaunchDescription()
     ld.add_action(namespace_prefix_arg)
     ld.add_action(node_id_arg)
-    ld.add_action(rviz_node)
+
+    ld.add_action(priority_node)
     return ld
